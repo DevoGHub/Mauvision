d697af5ca4c1fb5b0eb0110b37a466a235f51c26
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2021-05-03 17:25:37 +0530

    Header completed, started with index.php

--- header.php
+++ header.php
@@ -1,2 +1,150 @@
 <header>
+  <div class="headerdiv">
+    <div class="icondiv">
+      <a href="index.php" class="logoiconanchor">
+        <div class="logoicondiv">
+          <svg
+             xmlns:dc="http://purl.org/dc/elements/1.1/"
+             xmlns:cc="http://creativecommons.org/ns#"
+             xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+             xmlns:svg="http://www.w3.org/2000/svg"
+             xmlns="http://www.w3.org/2000/svg"
+             xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+             xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+             inkscape:version="1.0 (4035a4fb49, 2020-05-01)"
+             sodipodi:docname="logo_icon.svg"
+             viewBox="0 0 1972.9323 2915.0232"
+             height="2915.0232"
+             width="1972.9324"
+             id="logoiconsvg"
+             version="1.1">
+            <metadata
+               id="metadata839">
+              <rdf:RDF>
+                <cc:Work
+                   rdf:about="">
+                  <dc:format>image/svg+xml</dc:format>
+                  <dc:type
+                     rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+                  <dc:title></dc:title>
+                </cc:Work>
+              </rdf:RDF>
+            </metadata>
+            <defs
+               id="defs837" />
+            <sodipodi:namedview
+               inkscape:current-layer="g841"
+               inkscape:window-maximized="1"
+               inkscape:window-y="-9"
+               inkscape:window-x="-9"
+               inkscape:cy="1078.3043"
+               inkscape:cx="1169.2591"
+               inkscape:zoom="0.28462088"
+               showgrid="false"
+               id="namedview835"
+               inkscape:window-height="1001"
+               inkscape:window-width="1920"
+               inkscape:pageshadow="2"
+               inkscape:pageopacity="0"
+               guidetolerance="10"
+               gridtolerance="10"
+               objecttolerance="10"
+               borderopacity="1"
+               bordercolor="#666666"
+               pagecolor="#ffffff" />
+            <g
+               transform="translate(182.53915,481.01851)"
+               id="g841"
+               inkscape:label="Image"
+               inkscape:groupmode="layer">
+              <path
+                 id="logoiconpath"
+                 d="M 1111.8879,1741.6707 C 741.46711,1360.7768 438.45852,1049.0734 438.53548,1048.9965 c 0.19702,-0.197 477.22524,1.1811 480.43714,1.388 l 2.632,0.1694 214.91748,209.2759 c 118.2046,115.1017 214.9957,209.1976 215.0913,209.102 0.096,-0.096 0.6534,-154.8534 1.2396,-343.9062 0.5862,-189.05276 1.1296,-343.79626 1.2076,-343.87416 0.1586,-0.1586 436.1267,3.7906 436.3278,3.9525 0.07,0.057 -0.6283,244.22126 -1.5524,542.58826 -0.9242,298.3671 -1.9897,669.3016 -2.3678,824.299 -0.3781,154.9973 -0.7776,281.9034 -0.8878,282.0135 -0.1101,0.1102 -303.2717,-311.4402 -673.6925,-692.334 z M 20.500815,1169.7801 c -111.495995,-0.6992 -202.749135,-1.297 -202.784765,-1.3283 -0.0356,-0.031 -0.10763,-371.17486 -0.16,-824.76355 l -0.0952,-824.70676 3.40009,3.43822 c 4.02688,4.07206 1345.05226,1374.65343 1346.95156,1376.63823 l 1.3015,1.36 H 927.52742 685.94078 L 470.26079,691.68154 254.5808,482.94516 l -0.0802,344.10008 -0.0801,344.10006 -15.59987,-0.043 c -8.5799,-0.024 -106.82384,-0.6153 -218.319825,-1.3146 z"
+                 style="stroke-width:0.32" fill='#ff4500'/>
+            </g>
+          </svg>
+        </div>
+      </a>
+      <div class="navicondiv">
+        <svg
+           xmlns:dc="http://purl.org/dc/elements/1.1/"
+           xmlns:cc="http://creativecommons.org/ns#"
+           xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+           xmlns:svg="http://www.w3.org/2000/svg"
+           xmlns="http://www.w3.org/2000/svg"
+           xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+           xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+           inkscape:version="1.0 (4035a4fb49, 2020-05-01)"
+           sodipodi:docname="navigation drawer.svg"
+           viewBox="0 0 625.49884 567.93677"
+           height="567.93677"
+           width="625.49884"
+           id="naviconsvg"
+           version="1.1"
+           onclick="openNav()">
+          <metadata
+             id="metadata839">
+            <rdf:RDF>
+              <cc:Work
+                 rdf:about="">
+                <dc:format>image/svg+xml</dc:format>
+                <dc:type
+                   rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+                <dc:title></dc:title>
+              </cc:Work>
+            </rdf:RDF>
+          </metadata>
+          <defs
+             id="defs837" />
+          <sodipodi:namedview
+             inkscape:current-layer="g841"
+             inkscape:window-maximized="1"
+             inkscape:window-y="-9"
+             inkscape:window-x="-9"
+             inkscape:cy="351.66898"
+             inkscape:cx="104.65644"
+             inkscape:zoom="0.60791014"
+             showgrid="false"
+             id="namedview835"
+             inkscape:window-height="1001"
+             inkscape:window-width="1920"
+             inkscape:pageshadow="2"
+             inkscape:pageopacity="0"
+             guidetolerance="10"
+             gridtolerance="10"
+             objecttolerance="10"
+             borderopacity="1"
+             bordercolor="#666666"
+             pagecolor="#ffffff" />
+          <g
+             transform="translate(-17.190999,-57.333335)"
+             id="g841"
+             inkscape:label="Image"
+             inkscape:groupmode="layer">
+            <path
+               id="naviconpath"
+               d="m 56.626502,622.8335 c -26.22256,-8.42619 -43.30502,-36.09328 -38.68071,-62.64803 4.00173,-22.97961 19.83909,-40.35359 42.06066,-46.14157 11.72874,-3.05495 186.844708,-3.05495 198.573448,0 11.20992,2.91981 19.98521,8.11894 27.40374,16.23598 20.54132,22.4755 20.49168,55.23669 -0.1168,77.02705 -8.00056,8.45941 -14.98457,12.86962 -25.14906,15.88093 -6.36903,1.88687 -18.6644,2.13685 -102.00001,2.07379 -86.824458,-0.0657 -95.371908,-0.26899 -102.091268,-2.42815 z m 7.62006,-225.55311 c -29.44426,-4.9504 -51.07393,-34.86204 -46.36286,-64.11514 3.97434,-24.67839 23.0196,-43.84549 47.56006,-47.86435 10.55035,-1.72776 347.815138,-1.72776 358.365498,0 30.67832,5.02402 52.37374,34.3103 47.56006,64.20054 -3.97434,24.67839 -23.0196,43.84549 -47.56006,47.86435 -9.55492,1.56474 -350.229088,1.48384 -359.562698,-0.0855 z m -4.81388,-228.72374 c -10.66723,-2.86858 -19.58588,-8.24366 -26.82997,-16.16984 -20.54341,-22.47777 -20.49377,-55.234492 0.11672,-77.027049 8.00057,-8.459413 14.98459,-12.869626 25.14907,-15.88093 6.54926,-1.940266 32.57648,-2.145496 272.091338,-2.145496 239.51487,0 265.54208,0.20523 272.09135,2.145496 10.16449,3.011304 17.1485,7.421517 25.14907,15.88093 20.60841,21.79036 20.65805,54.551549 0.1168,77.027049 -3.5487,3.88282 -9.40071,8.66449 -13.00447,10.6259 -14.93018,8.12602 2.53686,7.66224 -284.96615,7.56622 -225.18704,-0.0752 -263.747668,-0.36412 -269.913758,-2.02228 z"
+               fill="#ff4500"style="stroke-width:1.33333" />
+          </g>
+        </svg>
+      </div>
+    </div>
+    <div class="navdiv">
+      <div class="navbtndiv">
+        <a href="index.php">
+          HOME
+        </a>
+      </div>
+      <div class="navbtndiv">
+        <a href="#">
+          LISTEN
+        </a>
+      </div>
+      <div class="navbtndiv">
+        <a href="#">
+          CONTACT
+        </a>
+      </div>
+    </div>
+  </div>
 </header>
